fdd0c1e278ed1aecd9c7294e2d023c4cfd713de9
JaydeepGaikwad0 <jaydeep.gaikwad.it19@ggits.net>
2025-05-12 14:08:39 +0530

    Your update message

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    port: 3000
   }
 })
